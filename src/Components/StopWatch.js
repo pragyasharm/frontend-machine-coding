7cee4138b0fff4bcccc79e2fa650fabeafa4de19
pragyasharm <pragya773767@gmail.com>
2024-04-06 16:58:43 +0530

    useEffect for minutes

--- src/Components/StopWatch.js
+++ src/Components/StopWatch.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const StopWatch = () => {
-    const [seconds, setSeconds] = useState(0);
+    const [seconds, setSeconds] = useState(56);
     const [minutes, setMinutes] = useState(0);
     const [toggleStart, setToggleStart] = useState(true);
 
-    const [intervalval, setIntervalval] = useState()
+    useEffect(()=> {
+       
+        const intervalId = setInterval(() => {
+          return !toggleStart && (seconds === 59 ? (setMinutes((minutes)=> minutes+1), setSeconds(0)): setSeconds((seconds)=> seconds+1) )
+        }, 1000);
+    
+
+            return () => {
+                clearInterval(intervalId);
+            }
+
+    }, [toggleStart, seconds, minutes])
 
     const handleStartStop = () => {
-        
         setToggleStart(!toggleStart);
-        if(!toggleStart) {
-            clearInterval(intervalval);       
-        } else {
-        const intervalId = setInterval(() => {
-           {/* setMinutes((minutes) => Math.floor((seconds+1)/60)) */}
-            setSeconds((seconds) => seconds + 1)
-        }, 1000);
-        setIntervalval(intervalId);
-    }
     }
 
     const handleReset = () => {
-        clearInterval(intervalval);
         setToggleStart(false);
-        setSeconds(0)
+        setSeconds(56)
     }
 
     return (
         <>
             <div>Stop Watch</div>
-            <div>{minutes < 10 ? <><span>0{minutes}</span></> : <><span>{minutes}</span></>} : {seconds < 10 ? <><span>0{seconds}</span></> : <><span>{seconds}</span></>}</div>
-            <button onClick={handleStartStop}>{toggleStart ? <>Start</> : <>Stop</>}</button>
+            <div>{minutes < 10 ? <span>0{minutes}</span> : <span>{minutes}</span>} : {seconds < 10 ? <span>0{seconds}</span> : <span>{seconds}</span>}</div>
+            <button onClick={handleStartStop}>{toggleStart ? "Start" : "Stop"}</button>
             <button onClick={handleReset}>Reset</button>
         </>
     )
